6ccfefa7f26c1e94c55058de03bd8a665411de0d
irockafe <isaac.rockafellow@gmail.com>
2017-12-13 14:46:55 -0500

    setup.py file for pyPI

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='isaac_pydoit_0.29',
+    packages = ['isaac_pydoit_0.29'],
+    version = '0.1',
+    description = ('A simple fork of pydoit version' +
+                   ' 0.29 that only considers a file changed only if' +
+                   ' that file is newer than the one listed in the pydoit' +
+                   ' database'
+                   ),
+    author = 'Isaac Rockafellow',
+    author_email = 'isaac.rockafellow@gmail.com'
+    url = 'https://github.com/irockafe/pydoit_0.29_fork'
+    download_url = ('https://github.com/irockafe/' +
+                    'pydoit_0.29_fork/archive/0.1.tar.gz'
+                    )
+    keywords = ['pydoit', 'doit', 'make']
+    classifiers = [],
+)
