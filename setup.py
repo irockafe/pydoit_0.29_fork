2d149602ae04526114c2bfadc9d1804d46c0a204
irockafe <isaac.rockafellow@gmail.com>
2017-12-31 09:15:32 -0800

    added uptodate checker that only looks for newer files

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='isaac_pydoit_0.29',
-    packages= ['doit'],  # ['isaac_pydoit_0.29'],
-    version='0.1',
+    packages=['doit'],  # ['isaac_pydoit_0.29'],
+    version='0.22',
     description=('A simple fork of pydoit version' +
                  ' 0.29 that only considers a file changed only if' +
                  ' that file is newer than the one listed in the pydoit' +
@@ -12,12 +12,10 @@ setup(
     author='Isaac Rockafellow',
     author_email='isaac.rockafellow@gmail.com',
     url='https://github.com/irockafe/pydoit_0.29_fork',
-    download_url=('https://github.com/irockafe/' +
-                  'pydoit_0.29_fork/archive/0.1.tar.gz'
-                  ),
     keywords=['pydoit', 'doit', 'make'],
     classifiers=['Programming Language :: Python :: 2.7',
                  ],
+    install_requires=['cloudpickle', 'six', 'configparser'],
     entry_points={'console_scripts': [
                       'doit = doit.__main__:main'
                                 ]
